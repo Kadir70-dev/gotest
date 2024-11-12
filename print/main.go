88b28d8f9ecc5962fdef5e9c2f4be8da6a6364a0
Kadir70-dev <kadirab1999@gamil.com>
2024-11-12 10:13:35 +0000

    first commit

---
+++ print/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main (){
+	age := 25
+	name := "Kadir"
+	height := 5.74
+	fmt.Println(age, name, height)
+	fmt.Printf("My name is %s and I am %d\n years old", name, age)
+}
