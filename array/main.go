749922e875a9f7b136f80c36665a31a8a0f921a2
Kadir70-dev <kadirab1999@gamil.com>
2024-11-13 11:32:06 +0000

    accessing index of array

--- array/main.go
+++ array/main.go
@@ -8,12 +8,13 @@ func main() {
 	var names [5]string
 
 	names[0] = "Kadir"
-	names[3] = "Kadir"
+	names[3] = "Mukadam"
 	// names[2] = "Kadir"
 	// names[3] = "Kadir"
 	// names[4] = "Kadir"
 	fmt.Println(names)
-	var numbers= [5]int{1, 2, 3, 4, 5}
+	var numbers= [9]int{1, 2, 3, 4, 5}
 	fmt.Println(numbers)
 	fmt.Println(len(numbers))
+    fmt.Println(names[3]) 
 }
