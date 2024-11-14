3967607014a14dab958bf70b273f3309b83b7d59
Kadir70-dev <kadirab1999@gamil.com>
2024-11-14 10:39:39 +0000

    infinite loop in go lang

--- for-loop/main.go
+++ for-loop/main.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("for loop in golang")
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
+	}
+
+	counter := 0
+	for {
+		fmt.Println("infinite loop")
+		counter++
 	}
 }
